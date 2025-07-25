bea374faeca8e136a4a90e02860980cc15120f9a
notacoham <alexcottam12@gmail.com>
2025-07-25 15:20:53 -0600

    add main functionality with a --create command and gitignore generate function

---
+++ src/ignore_cli/main.py
@@ -0,0 +1,42 @@
+import argparse
+import os
+
+def generate_gitignore():
+    gitignore_path = os.path.join(directory, '.gitignore')
+
+    try:
+        with open(gitignore_path, 'w') as f:
+            pass
+        print(f".gitignore file created at {gitignore_path}")
+    except IOError as e:
+        print(f"Error creating .gitignore file: {e}")
+
+def main():
+    parser = argparse.ArgumentParser(
+        description='Generate a .gitignore file in the specified directory.'
+    )
+
+    parser.add_argument(
+        'create',
+        action='store_true',
+        help='Create a .gitignore file in the specified directory.'
+    )
+
+    parser.add_argument(
+        'directory',
+        nargs='?',
+        default='.',
+        help='The directory where the .gitignore file will be created. Defaults to the current directory.'
+    )
+
+    args = parser.parse_args()
+
+    if args.create:
+        target_directory = os.path.abspath(args.directory)
+        if not os.path.isdir(target_directory):
+            print(f"Error: The specified directory '{target_directory}' does not exist.")
+            return
+        generate_gitignore(target_directory)
+    else:
+        print("No action specified.")
+        print("Use --help for more information.")
